4fc63d3a127e01829ed4bb06841bcd62eb5ade57
hannah <hannah@dagmar-neschen.de>
2019-06-15 23:25:26 +0200

    Wohnen hinzugefügt

--- runner.py
+++ runner.py
@@ -13,36 +13,46 @@ def hello_world():
 
 def fragebogen_generieren():
     fragebogen = []
-    frage = fragen.Question("Treiben Sie Sport?")
-    frage.add_answer('ja')
+    frage = fragen.Question("Treiben Sie Sport?", 0.35)
+    frage.add_answer('ja', faktor = 1.1)
     frage.add_answer('nein')
     fragebogen.append(frage)
 
-    frage = fragen.Question('Wie oft essen Sie Fleisch?')
-    frage.add_answer('täglich')
-    frage.add_answer('mehrmals wöchentlich')
-    frage.add_answer('einmal die Woche')
-    frage.add_answer('nie')
+    frage = fragen.Question('Wie oft essen Sie Fleisch?', 0.78)
+    frage.add_answer('täglich', faktor = 1.3)
+    frage.add_answer('mehrmals wöchentlich', faktor = 1)
+    frage.add_answer('einmal die Woche', faktor = 0.8)
+    frage.add_answer('nie', faktor = 0.7)
     fragebogen.append(frage)
 
-    frage = fragen.Question('Worauf achten sie bei der Anschaffung neuer Konsumgüter?')
-    frage.add_answer('Langlebigkeit')
+    frage = fragen.Question('Worauf achten sie bei der Anschaffung neuer Konsumgüter?', 1)
+    frage.add_answer('Langlebigkeit', faktor = 0.9)
     frage.add_answer('Funktionalität')
-    frage.add_answer('günstige Preise')
+    frage.add_answer('günstige Preise', faktor = 1.1)
     fragebogen.append(frage)
 
-    frage = fragen.Question('Wie schätzen sie ihr Kaufverhalten ein?')
-    frage.add_answer('sparsam')
+    frage = fragen.Question('Wie schätzen sie ihr Kaufverhalten ein?', 1)
+    frage.add_answer('sparsam', faktor = 0.9)
     frage.add_answer('durchschnittlich')
-    frage.add_answer('großzügig')
+    frage.add_answer('großzügig', faktor = 1.1)
     fragebogen.append(frage)
 
-    frage = fragen.Question('Sind Sie Halter*In eines Haustieres?')
-    frage.add_answer('Hund')
-    frage.add_answer('Katze')
-    frage.add_answer('Nagetier')
-    frage.add_answer('Nein')
+    frage = fragen.Question('Sind Sie Halter*In eines Haustieres?', 1)
+    frage.add_answer('kleiner Hund', faktor = 1.8)
+    frage.add_answer('großer Hund', faktor = 3)
+    frage.add_answer('Katze', faktor=2)
+    frage.add_answer('Alpaka', faktor = 6)
+    frage.add_answer('Nein', faktor = 0)
     fragebogen.append(frage)
+
+    frage = frage.Question('Auf wievielen Quadratmetern Wohnfläche leben Sie?', 1)
+    frage.add_answer('bis 40', faktor = 5.8)
+    frage.add_answer('40 - 60', faktor = 6.2)
+    frage.add_answer('60 - 90', faktor = 6.6)
+    frage.add_answer('90 - 120', faktor = 7.1)
+    frage.add_answer('ab 120', faktor = 8)
+    fragebogen.append(frage)
+
     return fragebogen
 
 #def antwort_generieren():
