868c816220224067db693255a52ed46616475d42
hannah <hannah@dagmar-neschen.de>
2019-06-15 17:37:26 +0200

    route '/senf' mit json-ausgabe eingebaut

--- runner.py
+++ runner.py
@@ -1,7 +1,29 @@
-
 from flask import Flask
+from flask import jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, JugendHackt!'
+    return '''Hello, JugendHackt!
+    
+    Den Senf kriegst du <a href="/senf">hier</a>.'''
+
+@app.route('/senf')
+def json_ausgabe():
+    d = get_data()
+    return jsonify(d)
+
+def get_data():
+    return {
+        'data':[
+            {
+                'frage':'',
+                'antworten':[
+                    '',
+                    '',
+                    ''
+                ]
+            },
+
+        ]
+    }
